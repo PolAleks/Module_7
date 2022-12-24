030c0bbb62aa198ad9486718bfd87405b05eb1a4
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-24 12:20:02 +0300

    7.1.5 - пример наследования Fruit и Vegetable от Food и далее по цепочке

---
+++ Food/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class Food { }
+
+    class Fruit : Food { }
+
+    class Vagetable : Food { }
+
+    class Apple : Fruit { }
+
+    class Banana : Fruit { }
+
+    class Pear : Fruit { }
+
+    class Potato : Vagetable { }
+
+    class Carrot : Vagetable { }
+}
