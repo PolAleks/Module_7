ecc79c869b0703938c1a90e51b1430d029c5ab93
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-24 12:10:13 +0300

    7.1.4 - Создание наследников ProjectManager и Developer для класса Employee

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee employee = new Employee();
         }
     }
     class Employee
