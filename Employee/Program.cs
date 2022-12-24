ec5c2001004c29ca7c227c9816132b0480ddde95
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-24 12:07:48 +0300

    Добавьте файлы проекта.

---
+++ Employee/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class Employee
+    {
+        public string name;
+        public int age;
+        public int salary;
+    }
+
+    class ProjectManager : Employee
+    {
+        public string projectName;
+    }
+
+    class Developer : Employee
+    {
+        public string programmingLanguage;
+    }
+}
