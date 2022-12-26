783a4e29e40f6e3382bbea5c28ca6a3d57bd8fee
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-26 16:33:09 +0300

    7.2.14 - индексатор типа int

---
+++ Indexer/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+
+        public int this[int index]
+        {   
+            get 
+            { 
+                if(index >= 0 && index < array.Length) 
+                    return array[index];
+                return 0;
+            }
+            set 
+            {
+                if (index >= 0 && index < array.Length)
+                    array[index] = value;
+            }
+        }
+    }
+}
