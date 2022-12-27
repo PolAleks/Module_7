74e1e81044a98f2107c5183eb424d1a7acd7b586
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-27 09:22:26 +0300

    7.3.3 - Работа абстрактного класса

---
+++ ComputerPart/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ComputerPart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+
+    class Processor : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Процессор");
+        }
+    }
+
+    class MotherBoard : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Материнская плата");
+        }
+    }
+
+    class GraphicCard : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Видеокарта");
+        }
+    }
+}
