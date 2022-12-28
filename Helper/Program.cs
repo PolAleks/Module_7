41732d0a721270f85d3dd21a79b61bfa31222168
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-28 12:00:37 +0300

    7.5.3 - Статический метод Swap меняющий переменные местами

---
+++ Helper/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 58;
+            Helper.Swap(ref num1,ref num2);
+        }
+    }
+    class Helper
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;            
+        }
+    }
+}
