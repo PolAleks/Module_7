52f23c61333f5b69ae092e6c38ce0da8854e3578
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-28 16:55:08 +0300

    7.6.7 - Добавление дженерик метода в скласс Car с универсальным параметром - часть авто

--- Car/Car.cs
+++ Car/Car.cs
@@ -6,17 +6,46 @@ using System.Threading.Tasks;
 
 namespace Car
 {
-    internal class Car<T>
+    internal class Car<TEngine>
     {
-        public T Engine;
+        public TEngine Engine;
+
+        public virtual void ChangePart<TPart>(TPart part) { }
     }
+
+    /// <summary>
+    /// Электрический двигатель
+    /// </summary>
     class ElectricEngine
     {
 
     }
-
+    /// <summary>
+    /// Бензиновый двигатель
+    /// </summary>
     class GasEngine
     {
         
+    }
+    /// <summary>
+    /// часть авто - аккумулятор
+    /// </summary>
+    class Battery
+    {
+
+    }
+    /// <summary>
+    /// часть авто - диференциал
+    /// </summary>
+    class Differential
+    {
+
+    }
+    /// <summary>
+    /// часть авто - колесо
+    /// </summary>
+    class Wheel
+    {
+
     }
 }
