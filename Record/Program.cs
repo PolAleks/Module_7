49d67989829bc31c3ddca8fa85e183b2045fef17
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-28 16:42:36 +0300

    7.6.6 - Класс дженерик с 2-мя универсальными параметрами

---
+++ Record/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+    class Record<TId, tValue>
+    {
+        public TId id;
+        public tValue value;
+        public DateTime date;
+    }
+}
