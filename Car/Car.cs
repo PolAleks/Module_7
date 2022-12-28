0334842956e5177e62c068fbbf34be9abcd4b2d4
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-28 16:27:16 +0300

    7.6.2 - Создание класса обобщения Car с универсальным типом двигателя.

---
+++ Car/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class Car<T>
+    {
+        public T Engine;
+    }
+    class ElectricEngine
+    {
+
+    }
+
+    class GasEngine
+    {
+        
+    }
+}
