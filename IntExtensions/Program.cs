c73daf51998ec10d90382b2b08a902b2a77618ff
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-28 12:58:36 +0300

    7.5.9 - Создание 2-ух методов расширения для Int

---
+++ IntExtensions/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IntExtensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+            int num2 = -13;
+            int num3 = 0;
+
+            Console.WriteLine(num1.GetNegative()); //-7
+            Console.WriteLine(num1.GetPositive()); //7
+            Console.WriteLine(num2.GetNegative()); //-13
+            Console.WriteLine(num2.GetPositive()); //13
+            Console.WriteLine(num3.GetNegative()); //0
+            Console.WriteLine(num3.GetPositive()); //0
+        }
+    }
+    static class IntExtensions
+    {
+        public static int GetNegative(this int num)
+        {
+            if (num <= 0) return num;
+            return -1 * num;
+        }
+
+        public static int GetPositive(this int num) 
+        {
+            if (num >= 0) return num;
+            return -1 * num;
+        }
+    }
+}
