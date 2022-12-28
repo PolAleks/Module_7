8037bd9943b96e0c79d8573ec880ec5fbd0a6429
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-28 11:49:06 +0300

    7.5.2 - Создание класса со статическим полем

---
+++ StaticField/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StaticField
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class Obj
+    {
+        public string name;
+        public string description;
+        public static int maxValue = 2000;
+    }
+}
